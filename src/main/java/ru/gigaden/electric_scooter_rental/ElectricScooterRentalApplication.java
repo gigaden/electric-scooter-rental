f84a2bad07f614221939257280c828eada77d950
gigaden <gigaden@ya.ru>
2026-04-07 23:02:57 +0300

    init: And how do you like this, Elon Musk?

---
+++ src/main/java/ru/gigaden/electric_scooter_rental/ElectricScooterRentalApplication.java
@@ -0,0 +1,13 @@
+package ru.gigaden.electric_scooter_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricScooterRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricScooterRentalApplication.class, args);
+	}
+
+}
